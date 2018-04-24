638edd2ce45790deb977370d5b3463b5df2c9e66
Robert Nowaczyk <iro8ert@o2.pl>
2018-04-24 19:34:10 +0200

    upgrade search

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function searchCountries() {
 	var countryName = $('#country-name').val();
 if(!countryName.length) countryName = 'Poland';
 $.ajax({
-	url: url + countryName,
+	url: url + countryName + url2,
 	method: 'GET',
 	success: showCountriesList
 });
@@ -17,6 +17,10 @@ function showCountriesList(resp) {
 	countriesList.empty();
 	resp.forEach(function(item) {
 		$('<li>').text(item.name).appendTo(countriesList);
+		$('#capitalName').text(item.name).appendTo(capitalCity)
 	})
 }
+
+var url2 = 'https://restcountries.eu/rest/v2/capital/'
+var capitalCity = $('#capital');
 
